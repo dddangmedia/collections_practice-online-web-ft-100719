21f0b8f615d7ee8a0f7a568bec8da2db742c60b4
Duong Dang <dddangmedia@gmail.com>
2019-10-18 02:50:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+def reverse_array(arr)
+ arr.reverse
+end
+
+def kesha_maker(arr)
+  arr.each_with_index do |word, i|
+    word[2] = "$"
+  end
+  arr
+end
+
+def find_a(arr)
+  arr.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(arr)
+  arr.inject{|acc, num| acc + num}
+end
 
